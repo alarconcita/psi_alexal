180a18532c3b51a357d66c237fef3b7bc70a2db5
Mariana Alarcón <124741193+alarconcita@users.noreply.github.com>
2024-10-04 06:12:36 -0300

    Add files via upload

---
+++ Psi_AlexAl/JS/cv.js
@@ -0,0 +1,10 @@
+document.getElementById('telefono').onclick=function(){
+    console.log("Capturamos el click")
+    document.getElementById('telefono').innerHTML = "+549 0341 156257370"
+}
+
+
+document.getElementById('email').onclick=function(){
+    console.log("Capturamos el click")
+    document.getElementById('email').innerHTML = "alarconrinaldial@gmail.com"
+}
